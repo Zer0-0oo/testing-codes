c27d31b79a2048c0f1afd4c2221384281f8c8af8
Zer0-0oo <mariarinha24@gmail.com>
2024-08-23 11:38:41 -0300

    Update learning in .java
    complete guide on the basics
    im learning too so if you wish

--- learning in .java
+++ learning in .java
@@ -20,9 +20,31 @@ a class name which must match the filename, and that every program must contain 
 linha apenas digite */
 /*types of variables:String - stores text, such as "Hello". String values are surrounded by double quotes
 int - stores integers (whole numbers), without decimals, such as 123 or -123
+you can also declar "final int" to make it unchangeable
 float - stores floating point numbers, with decimals, such as 19.99 or -19.99
 char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
-boolean - stores values with two states: true or false*/
+boolean - stores values with two states: true or false
+byte - stores whole values from -128 to 127(we will be calling whole "w"from now on)
+short - stores w from -32768 to 32767
+long - store w from -9223372036854775808 to 9223372036854775807(noting that it MUST end it with"L"if using long variable)
+double - basically float but stores bigger values lol
+NOTE(using "e" will idicate "power of 10" if using float/double)
+other examples
+int myNum = 5;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool = true;
+String myText = "Hello";
+
+declaring variables
+"variable"EX:STRING variablename="placeholder"
+or
+"variable"EX:CHAR variablename='S'
+a variable MUST have a unique name
+it cant contain any spaces(instead use"_"for when you need it) 
+you can declare one value to more than one variable
+EX: int x,y,z;
+    x=y=z="variable"EX:"50";*/
 public class Phrase{
   public static void phrase(string[]args){
     System.out.println("this should work")
