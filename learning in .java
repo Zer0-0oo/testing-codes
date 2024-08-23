89564a7d89755b7ae4e92006793ee6b2e31661a0
Zer0-0oo <mariarinha24@gmail.com>
2024-08-23 08:43:38 -0300

    Create learning in .java

---
+++ learning in .java
@@ -0,0 +1,30 @@
+public class Main{
+  public static void main(string[]args){
+    System.out.println("hello world");
+  }
+}
+
+
+/* just remember that every Java program has
+a class name which must match the filename, and that every program must contain the main() method.*/
+//every class MUST be associated to a file,like"Main"is a simulated file that every debugger has
+
+//"println" is the "print" of the java language.
+//note there is also the "print" method wich does not insert a new line at the end of output
+//to get a code to work you must type first"System.out.(placeholder);"example:
+//system.out.println("hello world");
+//system: built in java class that has useful members
+//remember ";" is the ":" of the java language every code has to end with it ";"
+//also remember,the curly braces"{}"mark the beginning and the end of a block code
+/*para adicionar comentarios apenas digite duas barras no inicio de uma linha e para usar mais de uma 
+linha apenas digite */
+/*types of variables:String - stores text, such as "Hello". String values are surrounded by double quotes
+int - stores integers (whole numbers), without decimals, such as 123 or -123
+float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+boolean - stores values with two states: true or false*/
+public class Phrase{
+  public static void phrase(string[]args){
+    System.out.println("this should work")
+      }
+}
